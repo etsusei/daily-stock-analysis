4dc126a11dc4899d0a1a875684a26550344a8198
etsusei <169111366+etsusei@users.noreply.github.com>
2026-01-24 16:38:27 +0900

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -23,7 +23,7 @@ yf.set_tz_cache_location(".yf_cache")  # 本地缓存时区信息
 load_dotenv()
 
 # ================= 用户配置区域 =================
-SYMBOLS = ["IONQ","AMSC", "OKLO","SMR","AVGO","LUMN","UEC","MRVL","NVDA"]# 股票代码列表
+SYMBOLS = ["IONQ","AMSC", "OKLO","SMR","NB","AVGO","LUMN","UEC","MRVL","NVDA"]# 股票代码列表
 API_KEY = os.getenv("GEMINI_API_KEY")  # 从环境变量读取API密钥
 NEWS_MODEL = "gemini-2.5-flash"        # 新闻搜索模型：2.5 Flash + Google Search
 ANALYSIS_MODEL = "gemini-3-flash-preview"    # 分析模型：Gemini 2.0 Flash 实验版（最新）
