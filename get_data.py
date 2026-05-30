5cef50c556fb7c1273d8d2c3718d5f2cb183810a
etsusei <169111366+etsusei@users.noreply.github.com>
2026-05-30 18:40:22 +0900

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -25,7 +25,7 @@ load_dotenv()
 # ================= 用户配置区域 =================
 SYMBOLS = ["IONQ","AMSC", "SNOW","NOW","NB","SMCI","RCAT","UEC","QCOM","NVDA"]# 股票代码列表
 API_KEY = os.getenv("GEMINI_API_KEY")  # 从环境变量读取API密钥
-NEWS_MODEL = "gemini-3.1-flash-lite"        # 新闻搜索模型：3.1 Flash + Google Search
+NEWS_MODEL = "gemini-2.5-flash"        # 新闻搜索模型：3.1 Flash + Google Search
 ANALYSIS_MODEL = "gemini-3.5-flash"    # 分析模型：Gemini 3.5 Flash （最新）
 # ===============================================
 
